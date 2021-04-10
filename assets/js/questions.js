2a8a77cebe098d561a716da04114956bd6f5425f
Edwin Troya <edtroyajr@gmail.com>
2021-04-09 23:50:20 -0700

    created linking pages to html and highscore and buttons

---
+++ assets/js/questions.js
@@ -0,0 +1,29 @@
+//Questions for the test
+var questions = [
+    {
+    question: "Inside which HTML element do we put the JavaScript?",
+    rightAnswer:"<script>",
+    wrongAnswers:["java", "Java.index", "scripting.index"],
+
+},
+{
+    question:"Which of the following would not create a varible",
+    rightAnswer: "function",
+    wrongAnswers: ["const","let","var"],
+},
+{
+    question:"Where is the correct place to insert a JavaScript?",
+    rightAnswer: "Both the <head> section and the <body> section are correct", 
+    wrongAnswers: ["The <body> section","The <head> section"],
+},
+{
+    question:"What is the correct syntax for referring to an external script called xxx.js?",
+    rightAnswer:"<script name=”xxx.js”>", 
+    wrongAnswers: ["<script href=”xxx.js”>","<script href=”xxx.js”>"],
+},
+{
+    question:"The external JavaScript file must contain the <script> tag",
+    rightAnswer:"False",
+    wrongAnswers: ["True"],
+},
+]
